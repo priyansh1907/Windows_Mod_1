2be45c9f7869b97aa158ebd6e89bb1e21de50185
priyansh gupta <43648245+priyansh1907@users.noreply.github.com>
2018-09-27 22:03:36 +0530

    Add files via upload

---
+++ Disable_WS_Temp_Prefetch.bat
@@ -0,0 +1,9 @@
+net.exe stop "Windows search"
+
+cd C:\Users\Priyansh Gupta\AppData\Local\Temp\
+del* /S /Q
+rmdir /S /Q "C:\Users\Priyansh Gupta\AppData\Local\Temp\"
+
+cd C:\Windows\Prefetch\
+del* /S /Q
+rmdir /S /Q "C:\Windows\Prefetch\"
